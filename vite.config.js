c23be262943dd17b4f0a1a7a33d2359451e52dc4
yuli aviram <yuli.aviram@nice.com>
2026-01-29 00:08:09 +0200

    65546645

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cyber-shinobi/',
+  base: '/cyber-shinobi-old/',
 })
